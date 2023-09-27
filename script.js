44e961981b4598d60bc2a707aac2376c152f2cd7
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2023-09-27 18:31:47 +0200

    added turn based marker system

--- script.js
+++ script.js
@@ -3,43 +3,38 @@ const marker_field = document.querySelectorAll(".marker-field");
 
 const gameboard = [];
 
+//player constructor
+function Player(sign) {
+    this.sign = sign;
+}
+
 //field constructor
-function gameboard_field(field, sign) {
+function gameboard_field(field) {
     this.field = field;
     this.taken = false;
-    this.sign = sign;
     let self = this
     this.field.onclick = function() {
-        console.log(self.sign)
         if (self.taken === false) {
-            self.field.innerHTML = self.sign;
+            self.field.innerHTML = t;
             self.taken = true;
-            console.log('hi')
+        }
+        if (t === player1.sign) {
+            t = player2.sign
+        } else if (t === player2.sign) {
+            t = player1.sign
         }
     };
-    
 };
-//generate a random mark(for testing)
-let randomMark = function() {
-    let num = Math.random();
-    if (num > 0.5) {
-        return 'X'
-    } else {
-        return 'O'
-    }
-};
+
+const player1 = new Player('X');
+const player2 = new Player('O');
+
+let t = player1.sign
+
+
 //push a gameboard field into the gameboard array
 for (let i = 0; i < marker_field.length; i++) {
-    let newField = new gameboard_field(marker_field[i], randomMark());
+    let newField = new gameboard_field(marker_field[i]);
     gameboard.push(newField);
     
 };
-// //overwrite innerHTML of a field (for testing)
-// function displayMarkers() {
-//     for (let i = 0; i < gameboard.length; i++) {
-//         if (gameboard[i].taken === false) {
-//             gameboard[i].field.innerHTML = gameboard[i].sign
-//             gameboard[i].taken = true
-//         }
-//     }
-// }
