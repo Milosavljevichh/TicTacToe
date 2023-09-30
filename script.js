1d403f54fa95df90acc176ff4394c6070a358634
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2023-09-30 18:01:13 +0200

    changed the structure of the script

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
 
-const marker_field = document.querySelectorAll(".marker-field");
+const Gameboard = (() => {
 
-const gameboard = [];
+    let gameboard = document.querySelector(".game-board");
+    let fieldsArr = [];
 
-//player constructor
-function Player(sign) {
-    this.sign = sign;
-}
-
-//field constructor
-function gameboard_field(field) {
-    this.field = field;
-    this.taken = false;
-    let self = this
-    //this functions was made so that players can't play in a 
-    //spot that's already taken, it also controls the 'players turn' system
-    this.field.onclick = function() {
-        if (self.taken === false) {
-            self.field.innerHTML = t;
-            self.taken = true;
-        }
-        if (t === player1.sign) {
-            t = player2.sign
-        } else if (t === player2.sign) {
-            t = player1.sign
-        }
+    const fieldFactory = (taken, mark) => {
+        return {taken, mark};
     };
-};
-
-const player1 = new Player('X');
-const player2 = new Player('O');
-
-let t = player1.sign
-
-
-//push a gameboard field into the gameboard array
-//gameboard array was made so that we can access the fields easily
-for (let i = 0; i < marker_field.length; i++) {
-    let newField = new gameboard_field(marker_field[i]);
-    gameboard.push(newField);
     
-};
+    const createGrid = () => {
+        
+        const turnHandler = (currentPlayer, nextPlayer, e, f) => {
+                e.innerHTML = currentPlayer.marker;
+                f.mark = currentPlayer.marker;
+                f.taken = true;
+                currentPlayer.showMarker();
+                currentPlayer.turn = !currentPlayer.turn;
+                nextPlayer.turn = !nextPlayer.turn;
+                checkForDraw();
+        };
+
+        for (i=0;i<9;i++) {
+            let newField = {
+                element: document.createElement("button"),
+                field: fieldFactory(false, '')
+            }
+            newField.element.classList.add("marker-field");
+            
+            newField.element.onclick = () => {
+                if (!newField.field.taken) {
+                    if (player1.turn) {
+                        turnHandler(player1, player2, newField.element, newField.field);
+                    } else if (player2.turn) {
+                        turnHandler(player2, player1, newField.element, newField.field);
+                    }
+                }}
+            gameboard.appendChild(newField.element);
+            fieldsArr.push(newField);
+        }};
+    
+    const playerFactory = (marker, turn) => {
+        const showMarker = () => console.log(`Next marker:${marker}`)
+        return {marker, turn, showMarker};
+    };
+
+    const player1 = playerFactory('X', true);
+    const player2 = playerFactory('O', false);
+
+    const checkForDraw = () => {
+
+        let result = fieldsArr.every(function (e) {
+            return e.field.taken;
+        });
+        
+        if (result) {
+            alert("draw");
+        }};
+
+    return {
+        createGrid,
+        playerFactory,
+        fieldsArr,
+        checkForDraw
+    }})();
+
+Gameboard.createGrid();
